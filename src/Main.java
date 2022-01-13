009e79bd8fdf984f15d80866f17134012b4eb7c0
janczyz <jan.czyz59@gmail.com>
2022-01-13 18:43:49 +0100

    Finished the calculator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[]args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+      boolean wylacz = false;
+        while(!wylacz) {
+            System.out.print("Podaj działanie które chcesz wykonać: (dodawanie/mnozenie/dzielenie/odejmowanie/wyjdz)");
+            String dzialanie = scanner.next();
+            if (dzialanie.equals("dodawanie")) {
+                double liczba1 = numer1(scanner);
+                double liczba2 = numer2(scanner);
+                System.out.println("Wynik: " + (liczba1 + liczba2));
+            }
+            if (dzialanie.equals("mnozenie")) {
+                double liczba1 = numer1(scanner);
+                double liczba2 = numer2(scanner);
+                System.out.println("Wynik: " + (liczba1 * liczba2));
+            }
+            if(dzialanie.equals("dzielenie")) {
+                double liczba1 = numer1(scanner);
+                double liczba2 = numer2(scanner);
+                System.out.println("Wynik: " + (liczba1 / liczba2));
+            }
+            if(dzialanie.equals("odejmowanie")) {
+                double liczba1 = numer1(scanner);
+                double liczba2 = numer2(scanner);
+                System.out.println("Wynik: " + (liczba1 - liczba2));
+            }
+            if(dzialanie.equals("wyjdz")) {
+                wylacz = true;
+            }
+        }
+    }
+    private static double numer1(Scanner scanner) {
+        System.out.println("Podaj pierwszą liczbę: ");
+        double liczba1 = scanner.nextDouble();
+        return liczba1;
+    }
+    private static double numer2(Scanner scanner) {
+        System.out.println("Podaj drugą liczbę: ");
+        double liczba2 = scanner.nextDouble();
+        return liczba2;
     }
 }
