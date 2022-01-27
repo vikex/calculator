5521786c5eaa407e85405abf5ce0bfd84e59d0d3
tomaszprzybylski <tomasz.przybylski@intive.com>
2022-01-27 17:49:51 +0100

    Pair added

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -5,40 +5,45 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-      boolean wylacz = false;
-        while(!wylacz) {
+        boolean wylacz = false;
+
+        Pair pair = new Pair();
+
+        while (!wylacz) {
             System.out.print("Podaj działanie które chcesz wykonać: (dodawanie/mnozenie/dzielenie/odejmowanie/wyjdz)");
             String dzialanie = scanner.next();
             if (dzialanie.equals("dodawanie")) {
-                double liczba1 = numer1(scanner);
-                double liczba2 = numer2(scanner);
-                System.out.println("Wynik: " + (liczba1 + liczba2));
+                pair.setLiczba1(numer1(scanner));
+                pair.setLiczba2(numer2(scanner));
+                System.out.println("Wynik: " + (pair.getLiczba1() + pair.getLiczba2()));
             }
             if (dzialanie.equals("mnozenie")) {
-                double liczba1 = numer1(scanner);
-                double liczba2 = numer2(scanner);
-                System.out.println("Wynik: " + (liczba1 * liczba2));
+                pair.setLiczba1(numer1(scanner));
+                pair.setLiczba2(numer2(scanner));
+                System.out.println("Wynik: " + (pair.getLiczba1() * pair.getLiczba2()));
             }
-            if(dzialanie.equals("dzielenie")) {
-                double liczba1 = numer1(scanner);
-                double liczba2 = numer2(scanner);
-                System.out.println("Wynik: " + (liczba1 / liczba2));
+            if (dzialanie.equals("dzielenie")) {
+                pair.setLiczba1(numer1(scanner));
+                pair.setLiczba2(numer2(scanner));
+                System.out.println("Wynik: " + (pair.getLiczba1() / pair.getLiczba2()));
             }
-            if(dzialanie.equals("odejmowanie")) {
-                double liczba1 = numer1(scanner);
-                double liczba2 = numer2(scanner);
-                System.out.println("Wynik: " + (liczba1 - liczba2));
+            if (dzialanie.equals("odejmowanie")) {
+                pair.setLiczba1(numer1(scanner));
+                pair.setLiczba2(numer2(scanner));
+                System.out.println("Wynik: " + (pair.getLiczba1() - pair.getLiczba2()));
             }
-            if(dzialanie.equals("wyjdz")) {
+            if (dzialanie.equals("wyjdz")) {
                 wylacz = true;
             }
         }
     }
+
     private static double numer1(Scanner scanner) {
         System.out.println("Podaj pierwszą liczbę: ");
         double liczba1 = scanner.nextDouble();
         return liczba1;
     }
+
     private static double numer2(Scanner scanner) {
         System.out.println("Podaj drugą liczbę: ");
         double liczba2 = scanner.nextDouble();
--- src/main/java/calculator/Pair.java
+++ src/main/java/calculator/Pair.java
@@ -1,4 +1,24 @@
 package calculator;
 
 public class Pair {
+    
+    double liczba1;
+    
+    double liczba2;
+    
+    public void setLiczba1(double liczba1) {
+        this.liczba1 = liczba1;
+    }
+
+    public void setLiczba2(double liczba2) {
+        this.liczba2 = liczba2;
+    }
+
+    public double getLiczba1() {
+        return liczba1;
+    }
+
+    public double getLiczba2() {
+        return liczba2;
+    }
 }
