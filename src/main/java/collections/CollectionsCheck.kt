4c030ebc0d342196c4e7b43234829a656ae704f7
tomaszprzybylski <tomasz.przybylski@intive.com>
2022-03-02 17:00:02 +0100

    Lesson 6: Collections

---
+++ src/main/java/collections/CollectionsCheck.kt
@@ -0,0 +1,19 @@
+package collections
+
+fun main() { 
+    val apple = "Apple"
+    val pear = "Pear"
+    val orange = "Orange"
+    
+    
+    //1. Add all to list and print all
+    
+    //2. Add all to set and print all
+    
+    //2.1 Add duplicates to set na print all
+    
+    //3. Add all to map and print all
+    
+    
+}
+    
